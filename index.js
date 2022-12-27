16b86b922d633107f05e517a00bca310d4da9509
zaltanzz <kashyapvipul997@gmail.com>
2022-12-27 21:27:39 +0530

    path edit

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-// const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+
+
+
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
@@ -38,17 +40,16 @@ app.get("/posts/:topics", (req, res) => {
   posts.forEach((element) => {
     const arrTitle = element.title;
     let arrBody = element.body;
-    arrBody=_.lowerCase(arrBody)
-    arrBody=arrBody.slice(0,100)
+    arrBody = _.lowerCase(arrBody);
+    arrBody = arrBody.slice(0, 100);
     if (arrTitle == tName) {
       console.log("match" + tName + arrBody);
-    } else 
-      console.log("no match" + tName + arrBody);
-    res.render('post',{tName:tName, arrBody:arrBody})
+    } else console.log("no match" + tName + arrBody);
+    res.render("post", { tName: tName, arrBody: arrBody });
   });
 });
 app.post("/compose", (req, res) => {
-  let body= req.body.postTitle;
+  let body = req.body.postTitle;
 
   let post = {
     title: req.body.postTitle,
@@ -59,8 +60,7 @@ app.post("/compose", (req, res) => {
 });
 
 app.listen(3000, function () {
-  console.log("Server started on port 3000");
+  console.log("Boom baby listening on port 3000.......");
 });
 
-
-module.exports=app; 
+module.exports = app;
